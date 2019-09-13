31efcad8cd414756b79d2d40a32f6ca47ffd2e3c
Charlotte Tregelles <charlotte@8thlight.com>
2019-09-13 14:20:31 -0400

    Refactor HTTPServer and Server so that user will ber able to call  to start their own server

--- src/main/java/io/github/lottetreg/Cuppa/Server.java
+++ src/main/java/io/github/lottetreg/Cuppa/Server.java
@@ -10,9 +10,10 @@ import java.util.function.BiPredicate;
 import java.util.stream.Stream;
 
 public class Server {
-  public void start(ServerSocket serverSocket) throws IOException {
+  public static void start(int portNumber, List<Routable> customRoutes) throws IOException {
+    ServerSocket serverSocket = new ServerSocket(new java.net.ServerSocket(portNumber));
+    List<Routable> routes = getAllRoutes(customRoutes);
     Connection connection;
-    List<Routable> routes = getRoutes();
 
     while ((connection = serverSocket.acceptConnection()) != null) {
       Thread thread = new Thread(new RunnableServer(connection, routes));
@@ -20,34 +21,20 @@ public class Server {
     }
   }
 
-  private List<Routable> getRoutes() {
-    List<Routable> routes = new ArrayList<>(Arrays.asList(
-        new Route("/simple_get", "GET", "ExampleController", "empty"),
-        new Route("/echo_body", "POST", "ExampleController", "echo"),
-        new Route("/method_options", "GET", "ExampleController", "empty"),
-        new Route("/method_options2", "GET", "ExampleController", "empty"),
-        new Route("/method_options2", "PUT", "ExampleController", "empty"),
-        new Route("/method_options2", "POST", "ExampleController", "empty"),
-        new Route("/pickles", "GET", "ExampleController", "pickles"),
-        new Route("/pickles_with_header", "GET", "ExampleController", "picklesWithHeader"),
-        new Redirect("/redirect", "GET", "/simple_get"),
-        new Route("/get_with_body", "HEAD", "", ""), // need this to pass acceptance tests >:(
-        new Route("/time", "GET", "ExampleController", "time")
-    ));
-
+  private static List<Routable> getAllRoutes(List<Routable> routes) {
     routes.addAll(defaultRoutes());
     routes.addAll(resourcesForCurrentDirectory());
 
     return routes;
   }
 
-  private List<Routable> defaultRoutes() {
+  private static List<Routable> defaultRoutes() {
     return Arrays.asList(
         new Resource("/", "GET", "/index.html")
     );
   }
 
-  private List<Routable> resourcesForCurrentDirectory() {
+  private static List<Routable> resourcesForCurrentDirectory() {
     List<Routable> resources = new ArrayList();
 
     BiPredicate<Path, BasicFileAttributes> isRegularFile =
